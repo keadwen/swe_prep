865c07e8c45219683c378f855814de1abcca070c
keadwen <jkbmlynarczyk@gmail.com>
2019-01-10 11:46:55 +1100

    [sort] Implement int insertion sort

---
+++ sorts/insert_sort.go
@@ -0,0 +1,18 @@
+// Package sorts contains various sorting algorithms. 
+package sorts
+
+// Insertion implements insertion sort algorithm for int slices.
+func Insertion(data []int) []int {
+	sorted := make([]int, len(data))
+	copy(sorted, data)
+
+	for i := 1; i < len(data); i++ {
+		j := i - 1;
+		item := sorted[i]
+		for ; j >= 0 && item < sorted[j]; j -= 1 {
+			sorted[j+1] = sorted[j]
+		}
+		sorted[j+1] = item
+	}
+	return sorted
+}
